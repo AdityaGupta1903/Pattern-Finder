e6a2c6bc87e824d732766b0b06c0e2d8ff5310da
Aditya <guptaditya19@gmail.com>
2025-12-02 10:04:38 +0530

    Added Html2Canvas code to capture the screenshot.

--- Client/public/content-script.js
+++ Client/public/content-script.js
@@ -1,11 +1,7 @@
 // content-script.js
+// const html2canvas = require("html2canvas");
+import html2canvas from "html2canvas";
 (function () {
-    const script = document.createElement('script');
-    script.src = 'https://html2canvas.hertzen.com/dist/html2canvas.js';
-    script.onload = () => {
-        console.log("Script loaded");
-    }
-    document.body.appendChild(script);
     console.log("Content script loaded");
     const title = document.querySelector("title")?.textContent ?? "No title";
     const links = [...document.querySelectorAll("a")]
@@ -15,9 +11,18 @@
     console.log("Page title:", title);
     console.log("First five links:", links);  
     chrome.runtime.sendMessage({ type: "PAGE_SUMMARY", title, links });
-    chorme.runtime.onMessage((msg)=>{
+     html2canvas(document.body).then((res)=>{
+          var imageData = res.toDataURL('image/png');
+          console.log(imageData);
+          var img = new Image();
+          img.src = imageData;
+        })
+    chrome.runtime.onMessage.addListener((msg)=>{
       console.log("Receive one message from the popup",msg);
       if(msg.type == "ScreenShot"){
+        html2canvas(document.body).then((res)=>{
+          console.log(res);
+        })
         /// Capture the Screen Shot and send back to popup.js and popup.js will forward this to the backend for analysis.
         console.log("Captured the screenshot was clicked");
       }
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills(),
     viteStaticCopy({
       targets: [
         {
@@ -19,7 +21,17 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
+        contentscript:"./public/content-script.js"
       },
+      output:{
+        entryFileNames : chunk=>{ /// require to bundle the contentscript which has 
+          if(chunk.name == "contentscript"){
+            return "content-script.js";
+          }
+          return "assets/[name].js"
+        },
+        assetFileNames:"assets/[name][extname]"
+      }
     },
   },
 });
